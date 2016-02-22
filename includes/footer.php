ec67684ff524432f80b605c6863f0b6d51873cc6
10858010 <dtt1@pct.edu>
2016-02-22 11:04:10 -0500

    assignment 2 initial commit

---
+++ includes/footer.php
@@ -0,0 +1,52 @@
+<!--Footer-->
+
+<footer>
+    
+      <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="footer_text">Stay Connected</h2>
+                </div>
+                <div class="col-lg-12" align="center">
+                   
+                <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-instagram fa-fw"></i> <span class="network-name">Instagram</span></a>
+                            </li>
+                              <li>
+                                <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-vine fa-fw"></i> <span class="network-name">Vine</span></a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            </li>
+                          
+                            
+                        </ul>
+                </div>
+            </div>
+
+        </div>
+    
+    
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12" align="center">
+                    
+                    <h2 class="footer_text">Lorem Ipsum</h2>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#" class="footer_text">Legal</a>
+                        </li>
+
+                    </ul>
+                    <p class="copyright text-muted small">Copyright &copy; My Company 2016. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    
+
+    
+    
+    </footer>
---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BWM399 Assignment #2</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Lorem Ipsum</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#products">Products</a>
+                    </li>
+                    <li>
+                        <a href="#newsletter">Newsletter</a>
+                    </li>
+                    <li>
+                        <a href="#contact.php">Contact</a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+
+    <!-- Header -->
+    <a name="#"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Lorem Ipsum</h1>
+                        <h3>A Fictitious Company</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-instagram fa-fw"></i><span class="network-name">Instagram</span></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-vine fa-fw"></i> <span class="network-name">Vine</span></a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+
+    <!-- Page Content -->
+
+    <a name="about"></a>
+    <div class="content-section-a">
+        <div class="container">
+
+
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <h2>About Us</h2>
+
+
+                    <p class="lead">Lorem Ipsum is a fictitous business that is proud to provide Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+
+
+
+                </div>
+            </div>
+
+
+
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="" src="img/star.png" width="140" height="140">
+                    <h2>Great Quality</h2>
+                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-md-4">
+                    <img class="" src="img/star.png" width="140" height="140">
+                    <h2>Great Prices</h2>
+                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-md-4">
+                    <img class="" src="img/star.png" width="140" height="140">
+                    <h2>Great Experience</h2>
+                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+            <!-- /.container -->
+
+        </div>
+
+    </div>
+    <!-- /.content-section-a -->
+    <a name="products"></a>
+    <div class="content-section-b">
+        <section>
+            <div class="container">
+                <h2>Products</h2>
+                <ul class="photos gallery-parent">
+                    <li>
+                        <a href="img/sun.png" data-title="Sun" data-footer="This is product 1" data-toggle="lightbox" data-gallery="mygallery" data-parent=".gallery-parent" title="This is image 1" data-placement="top"><img src="img/sun.png" class="img-thumbnail"></a>
+                    </li>
+                    <li>
+                        <a href="img/Earth.png" data-title="Earth" data-footer="This is product 2" data-toggle="lightbox" data-gallery="mygallery" data-parent=".gallery-parent" title="This is image 2"><img src="img/Earth.png" class="img-thumbnail"></a>
+                    </li>
+
+                    <li>
+                        <a href="img/star.png" data-title="Star" data-footer="This is product 3" data-toggle="lightbox" data-gallery="mygallery" data-parent=".gallery-parent" title="This is image 3" data-hover="Tooltip"><img src="img/star.png" class="img-thumbnail"></a>
+                    </li>
+
+                    <li>
+                        <a href="img/star.png" data-title="Star" data-footer="This is product 4" data-toggle="lightbox" data-gallery="mygallery" data-parent=".gallery-parent" title="This is image 3" data-hover="Tooltip"><img src="img/star.png" class="img-thumbnail"></a>
+                    </li>
+
+                    <li>
+                        <a href="img/Earth.png" data-title="Earth" data-footer="This is product 5" data-toggle="lightbox" data-gallery="mygallery" data-parent=".gallery-parent" title="This is image 2"><img src="img/Earth.png" class="img-thumbnail"></a>
+                    </li>
+                    <li>
+                        <a href="img/sun.png" data-title="Sun" data-footer="This is product 6" data-toggle="lightbox" data-gallery="mygallery" data-parent=".gallery-parent" title="This is image 1" data-placement="top"><img src="img/sun.png" class="img-thumbnail"></a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </section>
+    </div>
+
+    <a name="newsletter"></a>
+    <div class="content-section-a">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <h2 class="section-heading">Sign Up For Our Newsletter</h2>
+                    <p class="lead"><form class="form-horizontal">
+                    
+                    <div class="form-group">
+    <label for="name" class="col-sm-2 control-label">Name</label>
+    <div class="col-sm-10">
+      <input type="name" class="form-control" id="" placeholder="Name" maxlength="20">
+    </div>
+  </div>
+                    
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email" maxlength="20">
+    </div>
+  </div>
+  
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign Up</button>
+    </div>
+  </div>
+</form></p>
+                </div>
+                
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.content-section-b -->
+
+
+    <!-- /.content-section-a -->
+
+
+
+
+
+    <!-- /.container -->
+    <!-- /.banner -->
+    <a name="contact"></a>
+    <div class="container">
+        
+        
+        
+        <div class="row">
+                <div class="col-lg-12">
+
+                    <h2>Contact</h2>
+
+
+                    <p class="lead">You can get in contact with us by reaching out to any of our staff via email or Twitter. We do our best to respond to all messages in a timely fashion. Magna officia instituendarum, doctrina veniam eu nescius philosophari. Ad quis 
+                        voluptatibus, nam duis efflorescere. Ea veniam amet legam pariatur, veniam 
+                        ingeniis ubi enim ipsum, quamquam magna expetendis singulis si in deserunt 
+                        coniunctione, quae eu non anim nostrud, quae offendit iis quorum quorum id 
+                        aliquip instituendarum non pariatur te legam cupidatat non nisi minim. </p>
+                    
+
+
+
+                </div>
+            </div>
+        
+        
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <tbody>
+                    <tr>
+                        <td>Corey Smith</td>
+                        <td>Owner, CEO</td>
+                        <td>corey@loremipsum.com</td>
+
+                        <td><a href="#">Twitter</a></td>
+                    </tr>
+                    <tr>
+                        <td>Jeff Stanley</td>
+                        <td>Director of Marketing</td>
+                        <td>jeff@loremipsum.com</td>
+                        <td><a href="#">Twitter</a></td>
+                    </tr>
+                    <tr>
+                        <td>Colleen Bowes</td>
+                        <td>Graphic Designer</td>
+                        <td>colleen@loremipsum.com</td>
+                        <td><a href="#">Twitter</a></td>
+                        <tr>
+                            <td>Grace Clark</td>
+                            <td>Director of Social Media</td>
+                            <td>franny@loremipsum.com</td>
+                            <td><a href="#">Twitter</a></td>
+                        </tr>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+    <!-- Footer -->
+
+    <?php include 'includes/footer.php' ?>
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+
+
+        <script src="js/ekko-lightbox.js"></script>
+        <script>
+            $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
+
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip()
+            });
+
+            $(document).ready(function () {
+                $(window).scroll(function () { // check if scroll event happened
+                    if ($(document).scrollTop() > 700) { // check if user scrolled more than 50 from top of the browser window
+                        $(".navbar-fixed-top").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+                    } else {
+                        $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+                    }
+                });
+            });
+
+
+            $(function () {
+                $('a[href*="#"]:not([href="#"])').click(function () {
+                    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                        if (target.length) {
+                            $('html, body').animate({
+                                scrollTop: target.offset().top
+                            }, 1000);
+                            return false;
+                        }
+                    }
+                });
+            });
+        </script>
+
+
+
+</body>
+
+</html>
